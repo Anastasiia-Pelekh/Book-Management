45b49bc3b88f424f820414ab760c4d4d1e85b19a
Anastasiia Pelekh <pelekhanastasia98@gmail.com>
2024-08-27 14:23:04 +0300

    add search and fix styles

--- src/app/my-books/my-books.component.ts
+++ src/app/my-books/my-books.component.ts
@@ -3,17 +3,27 @@ import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { ManageBooksService } from '../shared/services/manage-books.service';
 import { Book } from '../shared/models/book.model';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialog } from '@angular/material/dialog';
 import { InfoBookDialogComponent } from '../info-book-dialog/info-book-dialog.component';
 import { animate, style, transition, trigger } from '@angular/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-my-books',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatIconModule, ],
+  imports: [
+    CommonModule, 
+    MatButtonModule, 
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule
+  ],
   templateUrl: './my-books.component.html',
   styleUrl: './my-books.component.scss',
   animations: [
@@ -30,6 +40,9 @@ import { animate, style, transition, trigger } from '@angular/animations';
 })
 export class MyBooksComponent implements OnInit {
   public bookList$!: Observable<Book[]>;
+  public filteredBooks$!: Observable<Book[]>;
+
+  public searchQuery: string = '';
 
   constructor(
     private router: Router,
@@ -40,6 +53,7 @@ export class MyBooksComponent implements OnInit {
 
   public ngOnInit(): void {
     this.bookList$ = this.manageBooksService.books$;
+    this.filteredBooks$ = this.bookList$;
   }
 
   public createBook(): void {
@@ -52,5 +66,14 @@ export class MyBooksComponent implements OnInit {
 
   public readMore(book: Book): void {
     this.dialog.open(InfoBookDialogComponent, { data: book });
+  }
+
+  public onSearch(): void {
+    this.filteredBooks$ = this.bookList$.pipe(
+      map(books => books.filter(book => 
+        book.title.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+        book.author.toLowerCase().includes(this.searchQuery.toLowerCase())
+      ))
+    );
   }
 }
